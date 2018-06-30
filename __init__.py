48108f82054639b7477dc493ff05745d2a71f460
Kenneth Motsoari <tken.motsoari@gmail.com>
2018-06-30 15:35:28 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,13 @@
 #this is a python file
+from flask import Flask
+from flask import render_template, request, session, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return render_template("home.html")
+
+@app.route("/blogs")
+def blogs():
+  return renter_template("blogs.html")
